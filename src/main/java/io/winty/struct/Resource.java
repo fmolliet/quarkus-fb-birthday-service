3632e41ccd9af0084171b8a1cfaf34af556fc25d
Fabio Molliet <fmolliet@hotmail.com>
2023-01-07 10:49:00 -0300

    feat: Criando metodos de atualização de dados e deletação

--- src/main/java/io/winty/struct/Resource.java
+++ src/main/java/io/winty/struct/Resource.java
@@ -2,12 +2,14 @@ package io.winty.struct;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.security.SecureRandom;
 import java.util.List;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.PATCH;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -16,11 +18,10 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.jboss.logging.Logger;
+import org.jboss.resteasy.reactive.RestResponse.Status;
 
 @Path("/birthday")
 public class Resource {
-    
-    public static final SecureRandom random = new SecureRandom();
     
     private static final Logger log = Logger.getLogger(Resource.class);
 
@@ -34,6 +35,7 @@ public class Resource {
     @Path("/{snowflake}")
     @Produces(MediaType.APPLICATION_JSON)
     public Birthday get(@PathParam("snowflake") String snowflake) {
+        log.info("BUSCANDO: " + snowflake);
         return Birthday.findBySnowflake(snowflake);
     }
     
@@ -41,8 +43,8 @@ public class Resource {
     @Consumes(MediaType.APPLICATION_JSON)
         @Transactional
     public Response create(CreateDTO request) throws URISyntaxException {
-        log.info(request);
-        random.nextBytes(new byte[8]);
+        log.info("CRIANDO: " + request);
+        
         Birthday birthday = new Birthday();
         
         birthday.setDay(request.day);
@@ -56,5 +58,40 @@ public class Resource {
                 new URI(String.format("/birthday/%s", birthday.getSnowflake())))
                 .entity(birthday)
                 .build();
+    }
+    
+    @PATCH
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(CreateDTO request){
+        log.info("ATUALIZANDO: " + request);
+        Birthday birthday = Birthday.findBySnowflake(request.snowflake);
+        
+        if ( birthday == null ){
+            throw new NotFoundException("Usuário não encontrado na base");
+        }
+        
+        birthday.setDay(request.day);
+        birthday.setMonth(request.month);
+        birthday.setName(request.name);
+        
+        birthday.update();
+        
+        return Response.ok(birthday).status(Status.ACCEPTED).build();
+    }      
+    
+    @DELETE
+    @Path("/{snowflake}")
+    public Response delete(  @PathParam("snowflake") String snowflake ){
+        log.info("DELETANDO: " + snowflake);
+        
+        Birthday birthday = Birthday.findBySnowflake(snowflake);
+        
+        if ( birthday == null ){
+            throw new NotFoundException("Usuário não encontrado na base");
+        }
+        
+        birthday.delete();
+        
+        return Response.noContent().build();
     }
 }
