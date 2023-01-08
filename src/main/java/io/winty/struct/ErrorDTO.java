c9c9bf3b63bfc733ee8723128cdfec6c6f738768
Fabio Molliet <fmolliet@hotmail.com>
2023-01-08 20:15:45 -0300

    feat: Adicionado Register for reflections

--- src/main/java/io/winty/struct/ErrorDTO.java
+++ src/main/java/io/winty/struct/ErrorDTO.java
@@ -1,11 +1,13 @@
 package io.winty.struct;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.Data;
 
 /**
  * ErrorDTO
  */
 @Data
+@RegisterForReflection
 public class ErrorDTO {
 
     private String message;
