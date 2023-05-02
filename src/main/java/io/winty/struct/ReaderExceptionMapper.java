e3d0c5d4d8ccc67d877e8de7cd483db387869348
fmolliet <fmolliet@hotmail.com>
2023-05-02 20:58:45 -0300

    feat: Migrando para quarkus 3

--- src/main/java/io/winty/struct/ReaderExceptionMapper.java
+++ src/main/java/io/winty/struct/ReaderExceptionMapper.java
@@ -1,12 +1,12 @@
 package io.winty.struct;
 
-import javax.ws.rs.BadRequestException;
-import javax.ws.rs.ClientErrorException;
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
-import javax.ws.rs.ext.Provider;
+import jakarta.ws.rs.BadRequestException;
+import jakarta.ws.rs.ClientErrorException;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 
 /**
  * ReaderExceptionMapper
--- src/main/java/io/winty/struct/Request.java
+++ src/main/java/io/winty/struct/Request.java
@@ -1,9 +1,9 @@
 package io.winty.struct;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 
 import lombok.ToString;
 
